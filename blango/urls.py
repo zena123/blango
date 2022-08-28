c882ce77a3b823b3fde94487ab42e2663b250ddb
Zainab Joumaa <eng.zainabjoumaa@gmail.com>
2022-08-28 10:09:15 +0000

    Finish html frameworks

--- blango/urls.py
+++ blango/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from blog.views import IndexTemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("", IndexTemplateView.as_view(), name="index"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
-# Create your views here.
+class IndexTemplateView(TemplateView):
+  template_name = 'blog/index.html'
