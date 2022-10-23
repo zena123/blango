990075c46f19c495cd0229459a0d7fe21a816011
Zainab Joumaa <eng.zainabjoumaa@gmail.com>
2022-10-23 06:01:50 +0000

    finish related fields

--- blog/api/views.py
+++ blog/api/views.py
@@ -1,8 +1,13 @@
 from rest_framework import generics
-
-from blog.api.serializers import PostSerializer
+from blango_auth.models import User
+from blog.api.serializers import PostSerializer,UserSerializer
 from blog.models import Post
 from blog.api.permissions import AuthorModifyOrReadOnly, IsAdminUserForObject
+
+class UserDetail(generics.RetrieveAPIView):
+    lookup_field = "email"
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
 
 
 class PostList(generics.ListCreateAPIView):
--- blog/models.py
+++ blog/models.py
@@ -4,7 +4,7 @@ from django.contrib.contenttypes.fields import GenericForeignKey, GenericRelatio
 from django.contrib.contenttypes.models import ContentType
 
 class Tag(models.Model):
-    value = models.TextField(max_length=100)
+    value = models.TextField(max_length=100, unique=True)
 
     def __str__(self):
         return self.value
