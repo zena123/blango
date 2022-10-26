7ae0d2576356aa9176228938f3f74b4f728e68d2
Zainab Joumaa <eng.zainabjoumaa@gmail.com>
2022-10-26 11:14:24 +0000

    finish viewsets and routers

--- blog/api/serializers.py
+++ blog/api/serializers.py
@@ -61,3 +61,9 @@ class PostDetailSerializer(PostSerializer):
             comment.save()
 
         return instance
+
+
+class TagSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tag
+        fields = "__all__"
--- blog/api/urls.py
+++ blog/api/urls.py
@@ -4,8 +4,10 @@ from drf_yasg.views import get_schema_view
 import os
 from rest_framework.urlpatterns import format_suffix_patterns
 from rest_framework.authtoken import views
+from rest_framework.routers import DefaultRouter
 
-from blog.api.views import PostList, PostDetail, UserDetail
+
+from blog.api.views import PostViewSet, UserDetail, TagViewSet
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -17,11 +19,12 @@ schema_view = get_schema_view(
     public=True,
 )
 
+router = DefaultRouter()
+router.register("tags", TagViewSet)
+router.register("posts", PostViewSet)
 
 
 urlpatterns = [
-    path("posts/", PostList.as_view(), name="api_post_list"),
-    path("posts/<int:pk>", PostDetail.as_view(), name="api_post_detail"),
     path("users/<str:email>", UserDetail.as_view(), name="api_user_detail")
 ]
 
@@ -41,6 +44,7 @@ urlpatterns += [
         schema_view.with_ui("swagger", cache_timeout=0),
         name="schema-swagger-ui",
     ),
+    path("", include(router.urls)),
 ]
 
 
