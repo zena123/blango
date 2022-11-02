2c824ac6c2d6ba002d5080af83f0dbd3ebc44ea0
Zainab Joumaa <eng.zainabjoumaa@gmail.com>
2022-11-02 10:08:45 +0000

    finish filtering

--- blog/api/urls.py
+++ blog/api/urls.py
@@ -25,7 +25,7 @@ router.register("posts", PostViewSet)
 
 
 urlpatterns = [
-    path("users/<str:email>", UserDetail.as_view(), name="api_user_detail")
+    path("users/<str:email>", UserDetail.as_view(), name="api_user_detail"),
 ]
 
 
@@ -45,6 +45,7 @@ urlpatterns += [
         name="schema-swagger-ui",
     ),
     path("", include(router.urls)),
+    path("posts/by-time/<str:period_name>/",PostViewSet.as_view({"get": "list"}),name="posts-by-time"),
 ]
 
 
