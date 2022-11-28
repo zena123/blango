5f5fd42148200eedcf47ac67b3ec3cbe44bfb94e
Zainab Joumaa <eng.zainabjoumaa@gmail.com>
2022-11-28 12:38:30 +0000

    Finish fetch and React hooks

--- blog/views.py
+++ blog/views.py
@@ -68,4 +68,7 @@ def get_ip(request):
 
 
 def post_table(request):
-    return render(request, "blog/post-table.html")
+
+  return render(
+      request, "blog/post-table.html", {"post_list_url": reverse("post-list")}
+  )
